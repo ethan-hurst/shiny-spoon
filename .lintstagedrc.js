59f13575b4ebbd93bb71091a030e066ef72e04d1
Ethan Hurst <ethan@hursty.co.nz>
2025-07-22 22:05:40 +0000

    fix: configure lint-staged to avoid import sorting parser errors

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,7 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  '*.{json,md,mdx,css,scss}': ['prettier --write'],
+  '*.{js,jsx,ts,tsx}': [
+    'eslint --fix',
+    'prettier --config .prettierrc.json --write',
+  ],
+  '*.{json,md,mdx,css,scss}': ['prettier --config .prettierrc.json --write'],
 }
