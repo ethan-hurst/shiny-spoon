bb8a24de9300834220802695520c7a43a4475d20
Ethan Hurst <ethan@hursty.co.nz>
2025-07-28 03:55:40 +0000

    chore: remove eslint fix from lint-staged configuration

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': [
-    'eslint --fix',
     'prettier --config .prettierrc.json --write',
   ],
   '*.{json,md,mdx,css,scss}': ['prettier --config .prettierrc.json --write'],
